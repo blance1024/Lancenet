a72512c057581dc1e1e9380ff7c6a2ae6b83644d
谢海棠 <1622323125@qq.com>
2019-02-25 15:46:39 +0800

    添加项目文件。

---
+++ Lanetnet.AspnetCore.APILog/MiddlewareExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Lanetnet.AspnetCore.APILog
+{
+    public static class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseResponseTime(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ResponseTimeMiddleware>();
+        }
+    }
+}
